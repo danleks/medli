5c28a16435a65838ccdae06e76e8324a8d309962
danleks <danilevich.lks@gmail.com>
2021-05-22 21:08:05 +0200

    Create MenuLinks.js

---
+++ src/utils/MenuLinks.js
@@ -0,0 +1,22 @@
+export const MENU_LINKS = [
+    {
+        title: 'Home',
+        to: '/#home'
+    },
+    {
+        title: 'About',
+        to: '/#about'
+    },
+    {
+        title: 'Courses',
+        to: '/#courses'
+    },
+    {
+        title: 'News',
+        to: '/#news'
+    },
+    {
+        title: 'Contact',
+        to: '/#contact'
+    }
+];
