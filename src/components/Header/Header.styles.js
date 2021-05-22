d099730cbc55393ba2ad0e6bab33adc8bc7708a0
danleks <danilevich.lks@gmail.com>
2021-05-22 21:05:48 +0200

    Create Header styles

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.header`
+    width: 100%;
+    padding: 3.5rem 2.4rem 0;
+    @media(min-width: 1109px) {
+        padding: 0;
+        padding-top: 2.7rem;
+    }
+`;
