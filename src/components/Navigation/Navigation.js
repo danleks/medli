d622ae1c538285682948d527cf260e6d42bd8c3b
danleks <danilevich.lks@gmail.com>
2021-05-22 21:07:10 +0200

    Create Navigation.js

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React, { useContext }  from 'react';
+import { MenuContext } from '../../contexts/menu-context';
+import { MENU_LINKS } from '../../utils/MenuLinks';
+import Button from '../Button/Button';
+import { NavigationStyles, NavigationWrapperStyles, NavigationInnerWrapperStyles, NavigationLinkStyles } from './Navigation.styles';
+
+const Navigation = () => {
+    const [menu] = useContext(MenuContext);
+    return (
+        <NavigationStyles open={menu}>
+            <NavigationWrapperStyles type='links'>
+                {MENU_LINKS.map((link, i) => <NavigationLinkStyles key={i} to={link.to}>{link.title}</NavigationLinkStyles>)}
+            </NavigationWrapperStyles>
+            <NavigationWrapperStyles type='button'>
+                <Button>Log in</Button>
+            </NavigationWrapperStyles>
+        </NavigationStyles>
+    )
+}
+
+export default Navigation;
