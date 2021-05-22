0926e31c425b31c16da63c1887ac2d6deb755eac
danleks <danilevich.lks@gmail.com>
2021-05-22 21:05:56 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 import 'normalize.css';
 import GlobalStyles from '../../styles/GlobalStyles';
 import Typography from '../../styles/Typography';
+import { MainWrapperStyles } from './Layout.styles';
 
 const Layout = ({ children }) => {
     return (
         <>
             <GlobalStyles />
             <Typography />
-            {children}
+            <MainWrapperStyles>
+                {children}
+            </MainWrapperStyles>
         </>
     )
 };
