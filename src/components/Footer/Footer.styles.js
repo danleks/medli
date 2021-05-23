556d3a2b54a7af19ca9009d6740166138759a42e
danleks <danilevich.lks@gmail.com>
2021-05-24 00:05:15 +0200

    Create Footer styles

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FooterStyles = styled.footer`
+    height: 93.8rem;
+    margin-top: 6.4rem;
+    background-color: var(--color-black);
+    @media(min-width: 1109px) {
+        height: 46.9rem;
+        margin-top: 11.4rem;
+    }
+`;
