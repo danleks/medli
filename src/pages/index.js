649c0d3d7c675fd2113d07fdc7e96089188665d3
danleks <danilevich.lks@gmail.com>
2021-05-23 15:00:09 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Hero from '../components/Hero/Hero';
 
 const index = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main>
+        <Hero />
+      </main>
+    </>
   )
 }
 
