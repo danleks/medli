161cb10a63620f4629e149d48bcb6595985e197c
danleks <danilevich.lks@gmail.com>
2021-05-24 00:05:07 +0200

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterStyles } from './Footer.styles';
+
+const Footer = () => {
+    return (
+        <FooterStyles>
+            footer
+        </FooterStyles>
+    )
+}
+
+export default Footer;
