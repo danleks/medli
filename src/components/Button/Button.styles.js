e4c0b03ba32d1c8d2885cb3b5deb39165f0d53df
danleks <danilevich.lks@gmail.com>
2021-05-24 00:04:35 +0200

    Update Button styles

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,12 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'gatsby';
 
 export const ButtonStyles = styled(Link)`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 12.4rem;
-    height: 5rem;
     border-radius: 8px;
     background-color: var(--color-orange);
     font-size: var(--font-size-l);
@@ -15,6 +13,18 @@ export const ButtonStyles = styled(Link)`
     &,
     &:visited {
         color: var(--color-black);
-        
+
     }
+    ${props => props.buttonstyle === 'login' && css`
+        width: 12.4rem;
+        height: 5rem;
+    `}
+    ${props => props.buttonstyle === 'cta' && css`
+        width: 100%;
+        height: 6.2rem;
+        @media(min-width: 768px) {
+            width: 25.5rem;
+            height: 7rem;
+        } 
+    `}
 `;
