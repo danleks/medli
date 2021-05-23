96624e6ac5c06176037bd145308b5e433a5c7110
danleks <danilevich.lks@gmail.com>
2021-05-24 00:04:26 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 import { ButtonStyles } from './Button.styles';
 
-const Button = ({ children }) => {
+const Button = ({style, type, children }) => {
+    const CustomTag = type === 'link' ? Link : 'button';
     return (
-        <ButtonStyles to="#">{ children }</ButtonStyles>
+        <ButtonStyles as={CustomTag} buttonstyle={style} to="#">{ children }</ButtonStyles>
     )
 };
 
-
 Button.propTypes = {
     children: PropTypes.node.isRequired,
+    type: PropTypes.oneOf(['link', 'button']).isRequired,
+    style: PropTypes.oneOf(['login', 'cta', 'submit']).isRequired,
 }; 
 
 export default Button;
