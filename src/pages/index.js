8248ee980fed477bd44ed7620a7883f5ae22bfa1
danleks <danilevich.lks@gmail.com>
2021-05-24 00:07:34 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
+import { ContentWrapperStyles } from '../styles/MainPage.styles';
+import Statistics from '../components/Statistics/Statistics';
+import Video from '../components/Video/Video';
+import Offer from '../components/Offer/Offer';
 
 const index = () => {
   return (
-    <>
-      <Header />
       <main>
-        <Hero />
+        <ContentWrapperStyles>
+          <Hero />
+          <Statistics />
+        </ContentWrapperStyles>
+        <Video />
+        <ContentWrapperStyles>
+          <Offer />
+        </ContentWrapperStyles>
       </main>
-    </>
   )
 }
 
