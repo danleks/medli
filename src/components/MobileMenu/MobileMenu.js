640a6360cb99927457e9b8462ad6ebec0a9e10bb
danleks <danilevich.lks@gmail.com>
2021-05-24 23:53:13 +0200

    Update MobileMenu.js

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { MenuContext } from '../../contexts/menu-context';
 import Logo from '../Logo/Logo';
 import Hamburger from '../Hamburger/Hamburger';
@@ -7,6 +7,11 @@ import { MobileMenuStyles, LinkStyles } from './MobileMenu.styles';
 
 const MobileMenu = () => {
     const [menu, setMenu] = useState(false);
+    useEffect(() => {
+        if (typeof window === 'undefined' || !window.document) {
+          return;
+        }
+      }, []);
     // remove scrolling when menu is open
     const body = document.querySelector('body');
     menu ? body.style.overflow = 'hidden' : body.style.overflow = 'scroll';    
