fadadcef72a3a4948de77d85c8781176b63039f7
danleks <danilevich.lks@gmail.com>
2021-05-24 23:16:19 +0200

    Update MobileMenu styles

--- src/components/MobileMenu/MobileMenu.styles.js
+++ src/components/MobileMenu/MobileMenu.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 export const MobileMenuStyles = styled.div`
     display: flex;
@@ -6,5 +7,12 @@ export const MobileMenuStyles = styled.div`
     align-items: center;
     @media(min-width: 1109px) {
         display: none;
+    }
+`;
+export const LinkStyles = styled(Link)`
+    &,
+    &:visited {
+        text-decoration: none;
+        cursor: pointer;
     }
 `;
