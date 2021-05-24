26845a2877f7f9c48bf36f0fd117547189e6f9f7
danleks <danilevich.lks@gmail.com>
2021-05-24 23:17:16 +0200

    Update formatNumber.js

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -2,5 +2,5 @@ export const formatNumber = (number) => {
     if (number > 1e6) {
         return new Intl.NumberFormat('en', { notation: 'compact' }).format(number); 
     }
-    return new Intl.NumberFormat().format(number); 
+    return new Intl.NumberFormat('en-IN').format(number); 
 };
